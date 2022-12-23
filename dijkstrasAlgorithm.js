4ca0bb3a0c60d8eb744e1ed2a31dc1142253860b
Logan Lampton <lampton.logan@gmail.com>
2022-12-23 17:45:46 -0500

    created class for simple PriorityQueue

--- dijkstrasAlgorithm.js
+++ dijkstrasAlgorithm.js
@@ -1,6 +1,9 @@
 // related to priority queue; uses that code
 // uses a weighted graph (the edges have values)
 
+// always initialize the shortest distance from A (starting vertex) to the other vertices as "infinity"
+    // set the distance to A from A as "0"
+
 class WeightedGraph {
     constructor() {
         this.adjacencyList = {}
@@ -13,4 +16,21 @@ class WeightedGraph {
         this.adjacencyList[vertex1].push({node: vertex2, weight});
         this.adjacencyList[vertex2].push({node: vertex1, weight});
     }
+}
+
+// Simple priority queue (PQ)
+class PriorityQueue {
+    constructor(){
+        this.values = [];
+    }
+    enqueue(val, priority){
+        this.values.push({val, priority});
+        this.sort();
+    };
+    dequeue(){
+        return this.values.shift();
+    };
+    sort(){
+        this.values.sort((a, b) => a.priority - b.priority)
+    };
 }
